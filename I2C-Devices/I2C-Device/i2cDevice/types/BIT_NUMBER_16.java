cd846d0dafd80e3e654ffda778b293cdf843b5b2
kamaSo-Macha <Stefan.Dickel@t-online.de>
2026-02-16 13:06:33 +0100

    Refactoring: Moved to new package and 16 bit type introduced.

---
+++ I2C-Devices/I2C-Device/i2cDevice/types/BIT_NUMBER_16.java
@@ -0,0 +1,73 @@
+/**
+ *
+ * **********************************************************************
+ * PROJECT       : I2C-Devices
+ * FILENAME      : BIT_NUMBER.java
+ *
+ * More information about this project can be found on Github
+ * http://github.com/kamaso-macha/I2C-Devices
+ *
+ * **********************************************************************
+ *
+ * Copyright (C)2025 by Stefan Dickel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ *
+ */
+
+
+package i2cDevice.types;
+
+/**
+ * Responsibilities:<br>
+ * 
+ * 
+ * <p>
+ * Collaborators:<br>
+ * 
+ * 
+ * <p>
+ * Description:<br>
+ * 
+ * 
+ * <p>
+ * @author Stefan
+ *
+ */
+
+// DOC
+// Created at 2026-01-25 16:59:56
+
+public enum BIT_NUMBER_16 {
+
+	B0,
+	B1,
+	B2,
+	B3,
+	B4,
+	B5,
+	B6,
+	B7,
+	B8,
+	B9,
+	B10,
+	B11,
+	B12,
+	B13,
+	B14,
+	B15
+	;
+	
+}
